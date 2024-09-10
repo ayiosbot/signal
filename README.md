c66d444c2c9586dbe66d338f016b2a0fb57ee681
DevAX1T <59183076+DevAX1T@users.noreply.github.com>
2024-09-10 16:15:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<div align="center">
+    <h1>@ayios/ipc<h1>
+    <h3><b>Ayios IPC System</b></h3>
+    <h6>This is primarily used internally by Ayios. It is free for anyone to use.</h6>
+</div>
+
+## Example
+Examples are not provided.
+
+## License
+All code within this repository created by Ayios is under MIT license. Other code within this repository, if present, is under its own respective license which will be displayed within their respective files.
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ayios. All rights reserved.
+ *  All code created by Ayios within this repository is licensed under the MIT License. Other
+ *  code not created by Ayios is under their respective license, which should have an
+ *  indication of whatever copyright the file is subject to.
+ *--------------------------------------------------------------------------------------------*/
+class SignalConnection<T> {
+    private readonly _symbol: Symbol;
+    private readonly _signal: Signal<T>;
+    constructor(signal: Signal<T>, symbol: Symbol) {
+        this._symbol = symbol;
+        this._signal = signal;
+    }
+    private get _listeners() {
+        return Object.getOwnPropertyDescriptor(this._signal, '_listeners')!.value as Map<Symbol, Function>;
+    }
+    get isConnected() {
+        return this._listeners.has(this._symbol);
+        // return this
+        // return false;
+    }
+    disconnect() {
+        this._listeners.delete(this._symbol);
+    }
+}
+
+export default class Signal<T = void> {
+    public readonly name?: string;
+    private readonly _listeners = new Map<Symbol, ((data: T) => void)>();
+    constructor(name?: string) {
+        this.name = name;
+    }
+    connect(callback: (data: T) => void): SignalConnection<T> {
+        const symbol = Symbol(this.name);
+        this._listeners.set(symbol, callback);
+        return new SignalConnection(this, symbol);
+    }
+    disconnectAll(exclusion: SignalConnection<T>) {
+        const exclusionSymbol = Object.getOwnPropertyDescriptor(exclusion, '_symbol')!.value;
+        this._listeners.forEach((_, symbol) => {
+            if (symbol !== exclusionSymbol) this._listeners.delete(symbol);
+        });
+    }
+    fire(data: T) {
+        this._listeners.forEach(listener => listener(data));
+    }
+}
