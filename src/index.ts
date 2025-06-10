c77ce734cd1d78b451472989fba922f871bf27a5
DevAX1T <darkweaponwaffles@gmail.com>
2025-06-09 21:56:17 -0700

    Fix listener issues

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,59 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Ayios. All rights reserved.
- *  All code created by Ayios within this repository is licensed under the MIT License. Other
- *  code not created by Ayios is under their respective license, which should have an
- *  indication of whatever copyright the file is subject to.
+ *  Copyright (c) 2025 Ayios. All rights reserved.
+ *  All code within this repository created by Ayios is under MIT license. Other code within
+ *  this repository is under its own respective license which will be displayed within their
+ *  respective files or around the areas of their code.
+ *  See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-class SignalConnection<T> {
-    private readonly _symbol: Symbol;
+import { Collection } from '@ayios/collection';
+
+export class SignalConnection<T> {
+    public readonly id: Symbol;
     private readonly _signal: Signal<T>;
     constructor(signal: Signal<T>, symbol: Symbol) {
-        this._symbol = symbol;
+        this.id = symbol;
         this._signal = signal;
     }
     private get _listeners() {
         return Object.getOwnPropertyDescriptor(this._signal, '_listeners')!.value as Map<Symbol, Function>;
     }
     get isConnected() {
-        return this._listeners.has(this._symbol);
+        return this._listeners.has(this.id);
     }
     disconnect() {
-        this._listeners.delete(this._symbol);
+        this._listeners.delete(this.id);
     }
 }
 
 export default class Signal<T = void> {
     public readonly name?: string;
-    private readonly _listeners = new Map<Symbol, ((data: T) => void)>();
+    private readonly _listeners = new Collection<Symbol, ((data: T) => void)>();
     constructor(name?: string) {
         this.name = name;
     }
-    async await() {
+    public async await() {
         return new Promise(resolve => {
             this.once(data => resolve(data));
         });
     }
-    once(callback: (data: T) => void) {
+    public once(callback: (data: T) => void) {
         const symbol = Symbol(this.name);
         this._listeners.set(symbol, (_data: T) => {
             this._listeners.delete(symbol);
             callback(_data);
         });
     }
-    connect(callback: (data: T) => void): SignalConnection<T> {
+    public connect(callback: (data: T) => void): SignalConnection<T> {
         const symbol = Symbol(this.name);
         this._listeners.set(symbol, callback);
         return new SignalConnection(this, symbol);
     }
-    disconnectAll(exclusion: SignalConnection<T>) {
-        const exclusionSymbol = Object.getOwnPropertyDescriptor(exclusion, '_symbol')!.value;
+    public disconnectAll(exclusion?: SignalConnection<T>) {
         this._listeners.forEach((_, symbol) => {
-            if (symbol !== exclusionSymbol) this._listeners.delete(symbol);
+            if (symbol !== exclusion?.id) this._listeners.delete(symbol);
         });
     }
-    fire(data: T) {
+    public fire(data: T) {
         this._listeners.forEach(listener => listener(data));
     }
 }
